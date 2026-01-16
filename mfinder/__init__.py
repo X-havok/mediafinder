fbe60327e019d20b64e3ac12271a346c7ed70818
X-havok <251760535+X-havok@users.noreply.github.com>
2026-01-16 11:23:54 +0530

    Update __init__.py

--- mfinder/__init__.py
+++ mfinder/__init__.py
@@ -11,18 +11,18 @@ load_dotenv()
 id_pattern = re.compile(r"^.\d+$")
 
 # vars
-APP_ID = os.environ.get("APP_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-DB_URL = os.environ.get("DB_URL", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+APP_ID = os.environ.get("APP_ID", "21575315")
+API_HASH = os.environ.get("API_HASH", "acdbe0573a6babdf1dfa88d113ed02c3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8244467113:AAEDT7JdLQ4MoTKSLtevvrSuP1peGf74aDA")
+DB_URL = os.environ.get("DB_URL", "postgresql://postgres:qizCtxi0LMCeo0VH@db.dbjedxgpalanniklrmyi.supabase.co:5432/postgres")
+OWNER_ID = int(os.environ.get("OWNER_ID", "8211074909"))
 ADMINS = [
     int(user) if id_pattern.search(user) else user
-    for user in os.environ.get("ADMINS", "").split()
+    for user in os.environ.get("ADMINS", "8211074909").split()
 ] + [OWNER_ID]
 DB_CHANNELS = [
     int(ch) if id_pattern.search(ch) else ch
-    for ch in os.environ.get("DB_CHANNELS", "").split()
+    for ch in os.environ.get("DB_CHANNELS", "-1003576663047").split()
 ]
 
 try:
@@ -40,3 +40,4 @@ HELP_KB = const.HELP_KB
 logging.config.fileConfig(fname="config.ini", disable_existing_loggers=False)
 LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
